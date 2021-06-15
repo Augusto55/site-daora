0504903b563153c14c2d541591f1462cb4e050fc
Augusto55 <augustotm5@gmail.com>
2021-06-14 21:10:54 -0300

    sa

--- registrar_inscricao.php
+++ registrar_inscricao.php
@@ -1,19 +1,22 @@
 <?php
 
-$nome = htmlspecialchars($_POST['nome'])
-$email = htmlspecialchars($_POST['email'])
-$nasc = htmlspecialchars($_POST['nasc'])
-$sexo = htmlspecialchars($_POST['sexo'])
+$nome = htmlspecialchars($_POST['nome']);
+$email = htmlspecialchars($_POST['email']);
+$nasc = htmlspecialchars($_POST['nasc']);
+$sexo = htmlspecialchars($_POST['sexo']);
 
 
 $nova_linha = "" . $nome . "," . $email . "," . $nasc . "," . $sexo . "\n";
 
 $arquivo = fopen('./registros.csv', 'a');
-fwrite($arquivo, $nova_linha);
-fclose($arquivo);
-
-echo "<script type='text/javascript'>alert('Inscrição recebida com sucesso. Obrigado ". $nome . "!');</script>";
-
+if ($arquivo) {
+  fwrite($arquivo, $nova_linha);
+  fclose($arquivo);
+  echo "<script type='text/javascript'>alert('Inscrição recebida com sucesso. Obrigado ". $nome . "!');</script>";
+}
+else {
+  echo "<script type='text/javascript'>alert('Erro na inscrição. Tente novamente ". $nome . "!');</script>";
+}
 include("index.html");
 
  ?>
