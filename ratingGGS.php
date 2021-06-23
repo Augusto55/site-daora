1898ebaccf51b7b87e7c65426c5018da8ff1ca3a
Augusto55 <augustotm5@gmail.com>
2021-06-23 17:28:16 -0300

    php try

---
+++ ratingGGS.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = htmlspecialchars($_POST['numeros']);
+$bsubmit = htmlspecialchars($_POST['botao_submit']);
+$media = 1;
+
+$media = ($numero + $media)/$bsubmit;
+
+include(G_ggs.html);
+?>
