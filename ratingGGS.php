6800c97a647dd1feaf80302bb138437be577f63a
Augusto55 <augustotm5@gmail.com>
2021-06-24 15:49:10 -0300

    a

--- ratingGGS.php
+++ ratingGGS.php
@@ -3,13 +3,12 @@
 $numero = htmlspecialchars($_POST['numeros']);
 $media = 1;
 
-$arquivo = fopen('./submit_ggs.txt', 'w');
-if ($arquivo){
-  $numerolido = fread($arquivo, "10");
-  $novonumero = ($numerolido + 1);
-  fwrite($arquivo, $novonumero);
-  echo "<script type='text/javascript'>alert($numero);</script>";
-}
+$arquivo = fopen('./submit_ggs.txt', 'w+');
+$numerolido = fread($arquivo, "10");
+$novonumero = ($numerolido + 1);
+fwrite($arquivo, $novonumero);
+echo "<script type='text/javascript'>alert($novonumero);</script>";
+
 
 
 
