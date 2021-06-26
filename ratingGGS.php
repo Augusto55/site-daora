beba1ddb542027fd8489266b17d21a8482ee382f
Augusto55 <augustotm5@gmail.com>
2021-06-26 20:14:28 -0300

    desisti do sistema de média

--- ratingGGS.php
+++ ratingGGS.php
@@ -1,17 +1,10 @@
 <?php
 
 $numero = htmlspecialchars($_POST['numeros']);
-$media = 1;
 
-$arquivo = fopen('./submit_ggs.txt', 'w+');
-$numerolido = fread($arquivo, "10");
-$novonumero = ($numerolido + 1);
-fwrite($arquivo, $novonumero);
-echo "<script type='text/javascript'>alert($novonumero);</script>";
-
-
-
-
-
-include("G_ggs.html");
+$arquivo = fopen('./submit_ggs.txt', 'a');
+if ($arquivo) {
+  fwrite($arquivo, $numero);
+  fclose($arquivo);
+}
 ?>
